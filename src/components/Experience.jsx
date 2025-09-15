8369b49e65035908c13ff2b61908ac4347a44144
Samritakhurana <Khurana.samrita@gmail.com>
2025-09-15 00:41:24 -0500

    portfolio website

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -8,10 +8,10 @@ const rawTimeline = [
     title: "Software Development Intern",
     date: "June 2025 - August 2025",
     description: [
-      "•	Designed and implemented a retrieval-augmented generation (RAG) pipeline with OpenAI embeddings and Elasticsearch, improving search relevance by ~35% while reducing average query latency from 220ms to 90ms.",
-      "•	Built Dockerized Python microservices to automate ingestion of 2,000+ daily documents into AWS S3, keeping data fresh and extending assistant query coverage by 45%. ",
-      "•	Prototyped a conversational AI assistant with database connectors and NLP pipelines, enabling employees to quickly find resources and leading to a 25% increase in engagement and a 20% drop in support tickets. ",
-      "•	Shadowed senior developers to explore performance profiling and contributed minor UI bug fixes.",
+      "Designed and implemented a retrieval-augmented generation (RAG) pipeline with OpenAI embeddings and Elasticsearch, improving search relevance by ~35% while reducing average query latency from 220ms to 90ms.",
+      "Built Dockerized Python microservices to automate ingestion of 2,000+ daily documents into AWS S3, keeping data fresh and extending assistant query coverage by 45%. ",
+      "Prototyped a conversational AI assistant with database connectors and NLP pipelines, enabling employees to quickly find resources and leading to a 25% increase in engagement and a 20% drop in support tickets. ",
+      "Shadowed senior developers to explore performance profiling and contributed minor UI bug fixes.",
     ],
   },
   {
@@ -19,29 +19,32 @@ const rawTimeline = [
     title: "Office Assistant (OA)",
     date: "September 2024 – Present",
     description: [
-      "•	Serve as the first point of contact at a 24/7 residence hall desk, supporting over 500+ residents with inquiries, key checkouts, guest access, and campus referrals.",
-      "•	Handle administrative operations including mail distribution, inventory tracking, and accurate use of internal systems and security protocols.",
-      "•	Commended for reliability and professionalism, maintaining a 95%+ resident satisfaction rating during active shifts.",
+      "Serve as the first point of contact at a 24/7 residence hall desk, supporting over 500+ residents with inquiries, key checkouts, guest access, and campus referrals.",
+      "Handle administrative operations including mail distribution, inventory tracking, and accurate use of internal systems and security protocols.",
+      "Commended for reliability and professionalism, maintaining a 95%+ resident satisfaction rating during active shifts.",
     ],
   },
   {
-    company: "VIP – Georgia Institute of Technology",
-    title: "Software Engineer | Research Assistant",
-    date: "01/06/2025",
+    company: "CSE Freshman Design Course- University of Minnesota",
+    title: "Stratospheric Balloon Spacecraft Project",
+    date: "January 2025 – May 2025",
     description: [
-      "Developing a Google-sponsored mobile app, collaborating with AI, design, and researcher teams under Dr. Starner to empower 10,000+ hearing parents to learn ASL and enhance communication.",
-      "Utilized C#, Figma, and Unity to improve sign detection accuracy by 30% through iterative algorithm tuning and user testing.",
-      "Designed an interactive platform featuring 250+ ASL vocabulary items.",
+      "Designed and built a lightweight spacecraft using CAD, microcontrollers, sensors, and rugged structural components for launch via high-altitude weather balloon (~91,000 ft).",
+      "Programmed and integrated onboard sensor systems, improving data accuracy through extensive ground testing, calibration cycles, and real-time error analysis.",
+      "Reduced overall system weight by 22%, enhancing altitude reach and payload stability during flight.",
+      "Applied engineering best practices to reduce power consumption by 30% through optimized microcontroller firmware and efficient wiring layouts.",
+      
     ],
   },
   {
-    company: "FPT Software Ho Chi Minh",
-    title: "Web Developer Intern",
-    date: "05/12/2024 – 08/01/2024",
+    company: "Envision, 4th Place @Empower Hacks 2.0",
+    title: "Virtual Hackathon",
+    date: "July 26 – August 2, 2024",
     description: [
-      "Collaborated with 20+ professionals to modernize legacy RPG AS400 systems into Java-based web apps using Spring Boot/MVC.",
-      "Built dynamic, responsive UIs with React, JavaScript, and SQL, significantly improving user experience.",
-      "Boosted app performance and maintainability by 65% through clean code practices and rigorous weekly testing.",
+      "Led the end-to-end development of Envision, a web platform designed to support international students through the application process, using HTML5, CSS3, and JavaScript.",
+      "Integrated features such as real-time chat, interactive checklists, and a dynamic map powered by OpenStreetMap, with a focus on performance and user accessibility. ",
+      "Created a persistent discussion forum and backend infrastructure to support high user engagement and platform reliability.",
+      "Facilitated 3+ workshops with peers and early users, gathering feedback to refine features and boost accessibility and usability by 25%."
     ],
   },
 ];
