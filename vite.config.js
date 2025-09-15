ccd3e25c35893c0adc9b4565a14d88bb073ab1ca
abdullahxyz85 <abdullahxyz85@gmail.com>
2025-09-15 09:37:30 +0500

    Make changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/My-Portfolio/",
   plugins: [react(), tailwindcss()],
-  build: { outDir: "docs" },
+  build: { outDir: "dist" },
 });
